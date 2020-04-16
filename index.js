31719733af9280d4463bdc73c0457f0721557a58
Scott Singer <ssinger303@gmail.com>
2020-04-16 17:36:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+//Objects updateObjectWithKeyAndValue(object, key, value)
+//      1) returns an object with the orignal key value pairs and the new key value pair
+//      2) does not modify the original object, but rather returns a clone with the new data
+//      3) returns an object with an updated key value pair
+
+//const recipe = { bacon: 3 }
+
+function updateObjectWithKeyAndValue(object,key,value){
+  return Object.assign({},object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key];
+}
