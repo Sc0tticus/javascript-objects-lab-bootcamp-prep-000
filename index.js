e6fac45eeb09066699e8a74953ea48567cabe00c
Scott Singer <ssinger303@gmail.com>
2020-04-16 18:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyDeleteFromObjectByKey(object,key){
   return object;
 }
 
-function destructivelyDeleteFromObjectsByKey(object,key){
+function deleteFromObjectsByKey(object,key){
     ObjNew = Object.assign({},object);
     delete ObjNew.key;
     return ObjNew;}
